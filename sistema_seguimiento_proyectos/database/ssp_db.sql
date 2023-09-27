245e75259e99a286294cab93f54be21fc9d22d57
Ricardo Donegana <109247941+RickyDonegana@users.noreply.github.com>
2023-09-27 14:55:16 -0300

    Add files via upload

---
+++ sistema_seguimiento_proyectos/database/ssp_db.sql
@@ -0,0 +1,66 @@
+-- Crear la base de datos
+CREATE DATABASE IF NOT EXISTS ssp_db;
+
+-- Usar la base de datos
+USE ssp_db;
+
+-- Tabla de Roles
+CREATE TABLE IF NOT EXISTS roles (
+    role_id INT AUTO_INCREMENT PRIMARY KEY,
+    role_name VARCHAR(50) NOT NULL
+);
+
+-- Insertar roles iniciales
+INSERT INTO roles (role_name) VALUES
+    ('Cliente'),
+    ('Desarrollador de sitios');
+
+-- Tabla de Usuarios
+CREATE TABLE IF NOT EXISTS usuarios (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    user_role ENUM('Cliente', 'Desarrollador de sitios') NOT NULL
+    -- Otras columnas para detalles adicionales de los usuarios
+);
+
+-- Tabla de Proyectos
+CREATE TABLE IF NOT EXISTS proyectos (
+    project_id INT AUTO_INCREMENT PRIMARY KEY,
+    project_name VARCHAR(255) NOT NULL,
+    start_date DATE NOT NULL,
+    estimated_delivery_date DATE NOT NULL
+    -- Otras columnas relevantes para detalles específicos del proyecto
+);
+
+-- Tabla de Etapas del Proyecto
+CREATE TABLE IF NOT EXISTS etapas_proyecto (
+    stage_id INT AUTO_INCREMENT PRIMARY KEY,
+    project_id INT NOT NULL,
+    stage_name VARCHAR(255) NOT NULL,
+    color VARCHAR(20) NOT NULL
+    -- Otras columnas relacionadas con detalles específicos de la etapa
+);
+
+-- Tabla de Cambios en el Proyecto
+CREATE TABLE IF NOT EXISTS cambios_proyecto (
+    change_id INT AUTO_INCREMENT PRIMARY KEY,
+    stage_id INT NOT NULL,
+    user_id INT NOT NULL,
+    change_date DATETIME NOT NULL,
+    change_description TEXT NOT NULL
+    -- Otras columnas para detalles adicionales del cambio
+);
+
+-- Agregar restricciones de clave externa
+ALTER TABLE etapas_proyecto
+ADD CONSTRAINT fk_etapas_proyecto_proyectos
+FOREIGN KEY (project_id) REFERENCES proyectos (project_id);
+
+ALTER TABLE cambios_proyecto
+ADD CONSTRAINT fk_cambios_proyecto_etapas_proyecto
+FOREIGN KEY (stage_id) REFERENCES etapas_proyecto (stage_id);
+
+ALTER TABLE cambios_proyecto
+ADD CONSTRAINT fk_cambios_proyecto_usuarios
+FOREIGN KEY (user_id) REFERENCES usuarios (user_id);
