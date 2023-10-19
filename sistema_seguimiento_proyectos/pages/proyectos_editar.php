01f4a2df89f14969fc564fa3d80d8b1ea1590973
Ricardo Donegana <109247941+RickyDonegana@users.noreply.github.com>
2023-10-19 09:50:58 -0300

    Add files via upload

--- sistema_seguimiento_proyectos/pages/proyectos_editar.php
+++ sistema_seguimiento_proyectos/pages/proyectos_editar.php
@@ -48,28 +48,28 @@ $pdo = conectarBaseDeDatos();
         <div id="editarProyectoForm" class="formulario-proyecto">
             <h2 class="titulo" id="formTitle">Editar Proyecto</h2>
             <form method="POST" id="proyectoForm">
-                <input type="hidden" class="input" id="id_proyecto_form" name="id_proyecto">
+                <input type="hidden" class="input" id="id_proyecto_form" name="id_proyecto" value="<?php echo $_GET['data-id']; ?>">
                 <label for="nombre_proyecto" class="label">Nombre del Proyecto:</label>
-                <input type="text" class="input" id="nombre_proyecto" name="nombre_proyecto" required>
+                <input type="text" class="input" id="nombre_proyecto" name="nombre_proyecto" required value="<?php echo $_GET['data-nombre']; ?>">
                 <label for="descripcion" class="label">Descripción:</label>
-                <textarea class="input" id="descripcion" name="descripcion" required></textarea>
+                <textarea class="input" id="descripcion" name="descripcion" required><?php echo $_GET['data-descripcion']; ?></textarea>
                 <label for="cliente" class="label">Cliente:</label>
-                <input type="text" class="input" id="cliente" name="cliente" required>
+                <input type="text" class="input" id="cliente" name="cliente" required value="<?php echo $_GET['data-cliente']; ?>">
                 <label for="desarrollador" class="label">Desarrollador:</label>
-                <input type="text" class="input" id="desarrollador" name="desarrollador" required>
+                <input type="text" class="input" id="desarrollador" name="desarrollador" required value="<?php echo $_GET['data-desarrollador']; ?>">
                 <label for="fecha_inicio" class="label">Fecha de Inicio:</label>
-                <input type="date" class="input" id="fecha_inicio" name="fecha_inicio" required>
+                <input type="date" class="input" id="fecha_inicio" name="fecha_inicio" required value="<?php echo $_GET['data-fechaInicio']; ?>">
                 <label for="fecha_entrega_estimada" class="label">Fecha de Finalización Estimada:</label>
-                <input type="date" class="input" id="fecha_entrega_estimada" name="fecha_entrega_estimada" required>
+                <input type="date" class="input" id="fecha_entrega_estimada" name="fecha_entrega_estimada" required value="<?php echo $_GET['data-fechaEntrega']; ?>">
                 <label for="estado" class="label">Estado:</label>
                 <select name="estado" class="select" id="estado_form" required>
-                    <option value="inicio">Inicio</option>
-                    <option value="planificacion">Planificación</option>
-                    <option value="ejecucion">Ejecución</option>
-                    <option value="supervision">Supervisión</option>
-                    <option value="cierre">Cierre</option>
+                    <option value="inicio" <?php echo ($_GET['data-estado'] === 'inicio') ? 'selected' : ''; ?>>Inicio</option>
+                    <option value="planificacion" <?php echo ($_GET['data-estado'] === 'planificacion') ? 'selected' : ''; ?>>Planificación</option>
+                    <option value="ejecucion" <?php echo ($_GET['data-estado'] === 'ejecucion') ? 'selected' : ''; ?>>Ejecución</option>
+                    <option value="supervision" <?php echo ($_GET['data-estado'] === 'supervision') ? 'selected' : ''; ?>>Supervisión</option>
+                    <option value="cierre" <?php echo ($_GET['data-estado'] === 'cierre') ? 'selected' : ''; ?>>Cierre</option>
                 </select>
-                <button type="submit" data-id="<?php echo $proyecto["id_proyecto"]; ?>" data-nombre="<?php echo $proyecto["nombre_proyecto"]; ?>" data-descripcion="<?php echo $proyecto["descripcion"]; ?>" data-cliente="<?php echo $proyecto["cliente"]; ?>" data-desarrollador="<?php echo $proyecto["desarrollador"]; ?>" data-fechaInicio="<?php echo $proyecto["fecha_inicio"]; ?>" data-fechaEntrega="<?php echo $proyecto["fecha_entrega_estimada"]; ?>" data-estado="<?php echo $proyecto["estado"]; ?>" class="boton-agregarEditar" id="btnEditar" name="editar_proyecto">Editar</button>
+                <button type="submit" data-id="<?php echo $_GET['data-id']; ?>" data-nombre="<?php echo $_GET['data-nombre']; ?>" data-descripcion="<?php echo $_GET['data-descripcion']; ?>" data-cliente="<?php echo $_GET['data-cliente']; ?>" data-desarrollador="<?php echo $_GET['data-desarrollador']; ?>" data-fechaInicio="<?php echo $_GET['data-fechaInicio']; ?>" data-fechaEntrega="<?php echo $_GET['data-fechaEntrega']; ?>" data-estado="<?php echo $_GET['data-estado']; ?>" class="boton-agregarEditar" id="btnEditar" name="editar_proyecto">Editar</button>
             </form>
         </div>
     </main>
