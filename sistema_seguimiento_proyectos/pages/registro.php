4f35c7cf775dd7dd1d829f1ec4e329d6f9cad117
agusfd2004 <145380021+agusfd2004@users.noreply.github.com>
2023-11-02 20:03:51 -0300

    Add files via upload

---
+++ sistema_seguimiento_proyectos/pages/registro.php
@@ -0,0 +1,62 @@
+<?php
+require_once('../php/conn.php');
+require_once('../php/inicioSesion/_registro.php');
+$pdo = conectarBaseDeDatos();
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro | Maitco</title>
+    <link rel="shortcut icon" href="https://maitco.com/wp-content/uploads/2017/07/LOGO-CHICO-2.png" type="image/png">
+    <link rel="stylesheet" href="../assets/css/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.4.0/fonts/remixicon.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- registro -->
+    <section class="section__form">
+        <div class="div__form card-nr">
+            <div class="form__tittle">
+                <img src="https://maitco.com/wp-content/uploads/2017/07/LOGO-CHICO-2.png" alt="logo" class="img__form">
+                <h1 class="tittle__form">Maitco</h1>
+            </div>
+            <h2 class="form__subtitle">Registro</h2>
+            <p class="form__paragraph">Ingresa tus datos aquí</p>
+            <form action="" method="post" autocomplete="off">
+                <div class="input__box">
+                    <input type="text" class="input" name="nombre_completo" placeholder="Nombre Completo" required="required">
+                    <input type="email" class="input" name="email" placeholder="Email" required="required">
+                    <input type="password" class="input" name="contrasena" placeholder="Contraseña" required="required">
+                    <input type="password" class="input" name="confirm_contrasena" placeholder="Confirmar contraseña" required="required">
+                    <label for="user_role" class="input">Rol</label>
+                    <select name="user_role" id="user_role" class="input" required="required">
+                        <option class="input" value="" selected disabled>Seleccionar rol</option>
+                        <option class="input" value="Cliente">Cliente</option>
+                        <option class="input" value="Desarrollador de sitios">Desarrollador de sitios</option>
+                    </select>
+                    <label class="input__check">
+                        <input type="checkbox" required="required"> Acepto las <a href="#">Condiciones de uso</a> y <a href="#">la política de privacidad</a>
+                    </label>
+                    <div class="btn__container">
+                        <button type="submit" class="input__btn">Registrarse</button>
+                    </div>
+                </div>
+            </form>
+            <p class="form__link">
+                ¿Ya tienes una cuenta?
+                <a href="../pages/login.php" class="text-link">Iniciar sesión</a>
+            </p>
+            <?php if (!empty($mensajeError)) : ?>
+                <p class="error-message">
+                    <?php echo $mensajeError; ?>
+                </p>
+            <?php endif; ?>
+        </div>
+    </section>
+</body>
+
+</html>
